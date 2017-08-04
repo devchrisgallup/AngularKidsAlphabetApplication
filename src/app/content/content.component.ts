934f411b61f316029c91d038f470c4eae3786cc2
devchrisgallup <chris.gallup@hotmail.com>
2017-08-04 17:53:52 -0500

    added more images

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -48,6 +48,31 @@ export class ContentComponent implements OnInit {
 
   // innerHTML
   public Adisplay = 'A'; 
+  public Bdisplay = 'B'; 
+  public Cdisplay = 'C'; 
+  public Ddisplay = 'D'; 
+  public Edisplay = 'E'; 
+  public Fdisplay = 'F'; 
+  public Gdisplay = 'G'; 
+  public Hdisplay = 'H'; 
+  public Idisplay = 'I'; 
+  public Jdisplay = 'J'; 
+  public Kdisplay = 'K'; 
+  public Ldisplay = 'L'; 
+  public Mdisplay = 'M'; 
+  public Ndisplay = 'N'; 
+  public Odisplay = 'O'; 
+  public Pdisplay = 'P'; 
+  public Qdisplay = 'Q'; 
+  public Rdisplay = 'R'; 
+  public Sdisplay = 'S'; 
+  public Tdisplay = 'T'; 
+  public Udisplay = 'U'; 
+  public Vdisplay = 'V'; 
+  public Wdisplay = 'W'; 
+  public Xdisplay = 'X'; 
+  public Ydisplay = 'Y'; 
+  public Zdisplay = 'Z';  
 
   // determin which value was passed to 
   // play the correct sound
@@ -60,54 +85,154 @@ export class ContentComponent implements OnInit {
       }, 2000);
     } else if (item == 'b') {
       this.b.play(); 
+      this.Bdisplay = "<img src='./assets/img/banana.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000);
     } else if (item == 'c') {
-      this.c.play(); 
+      this.c.play();
+      this.Cdisplay = "<img src='./assets/img/cat.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'd') {
-      this.d.play(); 
+      this.d.play();
+      this.Ddisplay = "<img src='./assets/img/dog.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'e') {
       this.e.play(); 
+      this.Edisplay = "<img src='./assets/img/elephant.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'f') {
-      this.f.play(); 
+      this.f.play();
+      this.Fdisplay = "<img src='./assets/img/family.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000);  
     } else if (item == 'g') {
       this.g.play(); 
+      this.Gdisplay = "<img src='./assets/img/goat.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'h') {
       this.h.play(); 
+      this.Hdisplay = "<img src='./assets/img/horse.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'i') {
       this.i.play(); 
+      this.Idisplay = "<img src='./assets/img/igloo.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'j') {
       this.j.play(); 
+      this.Jdisplay = "<img src='./assets/img/jacket.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'k') {
       this.k.play(); 
+      this.Kdisplay = "<img src='./assets/img/boy.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'l') {
       this.l.play(); 
+      this.Ldisplay = "<img src='./assets/img/legs.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'm') {
       this.m.play(); 
+      this.Mdisplay = "<img src='./assets/img/man.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'n') {
       this.n.play(); 
+      this.Ndisplay = "<img src='./assets/img/nut.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'o') {
       this.o.play(); 
+      this.Odisplay = "<img src='./assets/img/open.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'p') {
       this.p.play(); 
+      this.Pdisplay = "<img src='./assets/img/pop.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'q') {
       this.q.play(); 
+      this.Qdisplay = "<img src='./assets/img/quilt.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'r') {
       this.r.play(); 
+      this.Rdisplay = "<img src='./assets/img/run.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 's') {
       this.s.play(); 
+      this.Sdisplay = "<img src='./assets/img/stop.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 't') {
       this.t.play(); 
+      this.Tdisplay = "<img src='./assets/img/trump.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 3000); 
     } else if (item == 'u') {
       this.u.play(); 
+      this.Udisplay = "<img src='./assets/img/up.png' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'v') {
       this.v.play(); 
+      this.Vdisplay = "<img src='./assets/img/violin.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'w') {
       this.w.play(); 
+      this.Wdisplay = "<img src='./assets/img/whale.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'x') {
       this.x.play(); 
+      this.Xdisplay = "<img src='./assets/img/xray.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 3000); 
     } else if (item == 'y') {
       this.y.play(); 
+      this.Ydisplay = "<img src='./assets/img/yellow.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else if (item == 'z') {
       this.z.play(); 
+      this.Zdisplay = "<img src='./assets/img/zebra.jpg' />";
+      setTimeout(() => {
+        location.reload(); 
+      }, 2000); 
     } else {
       console.log('Opps! Looks like there was a coding error, sorry.'); 
     }
